b08465f224c5c6a1e43791ce99ffee03d0b8c6a7
Belotti17 <belottiwenze@gmail.com>
2025-09-25 00:32:07 +0100

    I wrote a small script that uses the to store a temperature in Celsius, converts it to Fahrenheit

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,14 @@
 // Exercice 5
 // Écrivez votre code ici
+
+// Déclaration de la température en Celsius
+
+let celsius = 25;
+
+// Conversion en Fahrenheit
+
+let fahrenheit = (celsius * 9) / 5 + 32;
+
+// Affichage du résultat
+
+console.log(celsius + "°C = " + fahrenheit + "°F");

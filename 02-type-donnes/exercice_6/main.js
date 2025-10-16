1a72880198a629c9982f189d5aae033b5a4a20f1
Belotti17 <belottiwenze@gmail.com>
2025-10-16 20:01:08 +0100

    feat resolution of the exercice6

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,2 +1,16 @@
 // Exercice 6
 // Écrivez votre code ici
+
+let age = 20;
+let nom = "Belotti";
+let estConnecte = true;
+let valeurNulle = null;
+let inconnue;
+
+console.log("Âge :", age);
+console.log("Nom :", nom);
+console.log("Est connecté :", estConnecte);
+console.log("Valeur nulle :", valeurNulle);
+console.log("Inconnue :", inconnue);
+
+module.exports = { age, nom, estConnecte, valeurNulle, inconnue };

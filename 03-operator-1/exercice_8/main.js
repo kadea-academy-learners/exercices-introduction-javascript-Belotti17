cb5c585e3f4ad58393d0277eeaefb78cf89a1700
Belotti17 <belottiwenze@gmail.com>
2025-10-16 13:33:11 +0100

    Finalization of exercise 7 and 8

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,2 +1,19 @@
 // Exercice 8
 // Écrivez votre code ici
+
+const salaireMensuel = 500;
+const loyer = salaireMensuel * 0.3;
+const nouriture = salaireMensuel * 0.2;
+const transport = salaireMensuel * 0.1;
+let autreDepenses = 50;
+autreDepenses += 25;
+let totalDepenses = loyer + nouriture + transport + autreDepenses;
+let reste = salaireMensuel + totalDepenses;
+
+// Vérifier si le budget est bien
+
+if (reste >= 100) {
+  console.log("Budget bien géré");
+} else {
+  console.log("Attention, budget serré");
+}

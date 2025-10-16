144691e823ed3eca36b6f79a41ff6936284ba15c
Belotti17 <belottiwenze@gmail.com>
2025-10-16 20:51:25 +0100

    fix resolution of the bug

--- 01-variables/exercice_4/main.js
+++ 01-variables/exercice_4/main.js
@@ -1,10 +1,9 @@
 // Exercice 4
 // Écrivez votre code ici
 
-const a = 5;
-const b = 8;
-
-const somme = a + b;
+let a = 5;
+let b = 10;
+let somme = a + b;
 console.log(somme);
 
 module.exports = { a, b, somme };

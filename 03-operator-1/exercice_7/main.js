6004b2c05ee37ad3374206d5a79430f6aa0fd3f5
Belotti17 <belottiwenze@gmail.com>
2025-10-16 13:43:49 +0100

    first modification to exercice

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -5,3 +5,19 @@ let agePère = ageEnfant * 2;
 let ageMère = agePère - 5;
 let ageGrandPère = ageMère * 2 + ageEnfant / 2;
 let ageOncle = agePère + 10;
+
+// Affichage des résultats
+
+console.log("Âge de l’enfant :", ageEnfant, "ans");
+console.log("Âge du père :", agePère, "ans");
+console.log("Âge de la mère :", ageMère, "ans");
+console.log("Âge du grand-père :", ageGrandPère, "ans");
+console.log("Âge de l’oncle :", ageOncle, "ans");
+
+module.exports = {
+  ageEnfant,
+  agePère,
+  ageMère,
+  ageGrandPère,
+  ageOncle,
+};
